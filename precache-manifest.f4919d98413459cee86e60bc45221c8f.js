1a22a84342c6bc9e8e6f00c77d66c9ef54675bd6
Farhan Alam <farhan_w901@outlook.com>
2020-08-16 20:34:17 +0600

    Updates

---
+++ precache-manifest.f4919d98413459cee86e60bc45221c8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf261c47cd29f746068f32f67e6c250",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "127b2bc74c7bd7107277",
+    "url": "/react-todo/static/css/main.06f0b036.chunk.css"
+  },
+  {
+    "revision": "8dcbf354a008322ff7d6",
+    "url": "/react-todo/static/js/2.1663f7ab.chunk.js"
+  },
+  {
+    "revision": "127b2bc74c7bd7107277",
+    "url": "/react-todo/static/js/main.bf093c86.chunk.js"
+  },
+  {
+    "revision": "052fad23ca9f036fcf47",
+    "url": "/react-todo/static/js/runtime~main.79666361.js"
+  }
+]);
